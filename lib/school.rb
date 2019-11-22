06c5f0331ea815d3303574c61c3b2dc8258ee5eb
Nory Lazo <nlazo@icloud.com>
2019-11-22 04:12:42 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+class School # The School class would by referred to as the model in the domain model context
+
+  attr_accessor :roster, :student_name
+  attr_writer :grade
+  
+  def initialize(roster)
+    @roster = Hash.new
+  end 
+  
+  def add_student(student_name, grade)
+    # @roster[grade] = []
+    
+    # @roster[grade] << student_name
+    
+    if @roster[grade]
+      @roster[grade] << student_name
+    else 
+      @roster[grade] = [student_name]
+    end 
+  end
+  
+  def grade(grade_no)
+    @roster[grade_no]
+  end 
+  
+  def sort 
+    @roster.each do |grade, student_names|
+      @roster[grade] = student_names.sort
+    end 
+  end
+  
+end
